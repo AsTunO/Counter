a0f6428d5712495fde11139adc28010558b7f67d
Júlio Cesar <juliocesar27072004@outlook.com>
2021-05-20 14:31:27 -0300

    First version

---
+++ README.md
@@ -0,0 +1,39 @@
+# Counter
+
+Hello! Welcome, a project developed by students from UNIFACOL, Facol University, of the Information Systems course. For the achievement of grades in the discipline of Programming Logic.
+
+## JavaScript/Node.js
+
+### Windows 
+
+- ``Download in : https://nodejs.org/en/``
+
+### Linux
+
+- ``sudo apt-get update sudo apt-get install build-essential libssl-dev``
+
+- ``curl -sL*https://raw.githubusercontent.com/creationix/nvm/v0.31.0*/install.sh _-o installnvm.sh``
+
+- ``_bash installnvm.sh``
+
+- ``nvm ls-remote``
+
+- ``nvm install 7.3.0``
+
+- ``nvm use -version-``
+
+- ``node -v``
+
+### Mac-OS
+
+- ``Download in : https://nodejs.org/en/``
+
+## Licence
+
+Copyright (c) 2021 JULIO CESAR
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/act.js
@@ -0,0 +1,66 @@
+var a  = window.document.getElementById('bt1')
+
+            a.addEventListener('click',click)
+            a.addEventListener('mouseenter',down)
+            a.addEventListener('mouseout',out)
+
+        function click() {
+            
+            a.style.background = 'White'
+            a.style.color = 'Black'
+
+            let vs = window.document.getElementById('ip1')
+            let ve = window.document.getElementById('ip2')
+            let vt = window.document.getElementById('ip3')
+            let res = window.document.getElementById('d1')
+
+            if (vs.value.length == 0 || ve.value.length == 0 || vt.value.length == 0 ){
+                window.alert('Missing Elements')
+                res.innerHTML = ('Mistake')
+            }else{
+                
+                res.innerHTML = (`Counting  \u{2714} <br> `)
+                res.innerHTML += (`  \u{1F4CD}`)
+
+                let Nvs = Number(vs.value)
+                let Nve = Number(ve.value)
+                let Nvt = Number(vt.value)
+
+                if (Nvt <= 0){
+                    window.alert('Invalid Step , [Now Step = 1]')
+                    Nvt = 1
+                }
+                
+                //Crescente
+                if ( Nvs < Nve ){
+                    for (let counter = Nvs ; counter <= Nve ; counter += Nvt ){
+                        res.innerHTML += (` ${counter} \u{27A1} `)
+                    }
+                    
+
+                //Decrescente
+                }else{
+                    for (let counter = Nvs ; counter >= Nve ; counter -= Nvt ){
+                        res.innerHTML += (` ${counter} \u{27A1} `)
+                    }
+                    
+
+                }
+                res.innerHTML += (`\u{1F6A9}`)
+
+                
+            }
+
+        }
+        function down(){
+            
+            a.style.color = 'Green'
+        }
+        function out(){
+            
+
+            a.style.background = 'Black'
+            a.style.color = 'yellow'
+        }
+
+
